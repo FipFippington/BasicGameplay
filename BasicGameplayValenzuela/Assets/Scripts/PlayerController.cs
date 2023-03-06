334e9476415c707ea6e61aa3521bfed5547e1411
FipFippington <116121197+FipFippington@users.noreply.github.com>
2023-03-06 15:21:26 -0800

    Almost Done
    Added furries and samwich mechanics

--- BasicGameplayValenzuela/Assets/Scripts/PlayerController.cs
+++ BasicGameplayValenzuela/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //no fun
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if ( transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        //ZOOOOOM
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // samwich
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
