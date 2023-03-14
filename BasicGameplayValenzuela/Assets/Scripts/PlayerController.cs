79ccc264dcbf7e5ced182059a4fb00b2d867a9c0
FipFippington <116121197+FipFippington@users.noreply.github.com>
2023-03-14 15:14:20 -0700

    Yipppe
    Almost done finished first bonussy

--- BasicGameplayValenzuela/Assets/Scripts/PlayerController.cs
+++ BasicGameplayValenzuela/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float verticalInput;
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
@@ -27,9 +28,19 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if ( transform.position.z > 3)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 3);
+        }
+        if (transform.position.z < -5)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -5);
+        }
         //ZOOOOOM
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         // samwich
         if (Input.GetKeyDown(KeyCode.Space))
